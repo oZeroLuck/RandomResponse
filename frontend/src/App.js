5f892500ff6f3b04e7ce11648bfc382a9f88b017
oZeroLuck <anonimousmuffin@gmail.com>
2021-11-25 21:38:51 +0100

    Added close function by id

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,33 +1,52 @@
 import 'bootstrap/dist/css/bootstrap.min.css';
-import React, {useEffect, useState} from 'react';
+import React, {useState} from 'react';
 import './App.css';
-import {Container, Modal, ModalBody} from "reactstrap";
-import Draggable from 'react-draggable';
+import {Container} from "reactstrap";
+import {DragModal} from "./component/drag-modal";
 
 function App() {
 
+    const [counter, setCounter] = useState(0);
     const [modals, setModals] = useState([]);
 
     function createModal() {
+        const top = Math.random()*90 + '%';
+        const left = Math.random()*90 + '%';
+
         const temp = [];
         modals.forEach(x => temp.push(x))
-        temp.push('a');
+        temp.push({index: counter,
+            item: <DragModal modalId={counter}
+                             top={top}
+                             left={left}
+                             close={closeById}/>});
         setModals(temp);
+        setCounter(counter + 1);
     }
 
-    useEffect(() => {
-        console.log("effect", modals);
-    })
+    function closeAll() {
+        setModals([]);
+        setCounter(0);
+    }
+
+    function closeById(id) {
+        console.log(id);
+        const temp = [];
+        modals.forEach(x => temp.push(x));
+        temp.filter(function (x) {
+           return  x.id !== id;
+        });
+        setModals(temp);
+    }
 
     return (
         <div className="App">
             <Container>
-                <button type={"button"} onClick={() => createModal()}>Create Modal</button>
-                {modals.length > 0 ? modals.map(modal => {
+                <button className="z-999" type={"button"} onClick={() => createModal()}>Create Modal</button>
+                <button className="z-999" type={"button"} onClick={() => closeAll()}>Close All</button>
+                {modals.length > 0 ? modals.map(item => {
                     return (
-                            <Modal isOpen={true} backdrop={false}>
-                                <ModalBody>Hello</ModalBody>
-                            </Modal>
+                        item.item
                     )
                 }) : null
                 }
--- frontend/src/component/drag-modal.js
+++ frontend/src/component/drag-modal.js
@@ -1,14 +1,22 @@
-import {Modal, ModalBody, ModalHeader} from "reactstrap";
 import * as React from "react";
+import Draggable from "react-draggable";
 
 class DragModal extends React.Component {
 
     render() {
         return(
-            <Modal isOpen={true}>
-                <ModalHeader>Hello</ModalHeader>
-                <ModalBody>Hello</ModalBody>
-            </Modal>
+            <Draggable>
+                <div className="position-absolute card w-sm bg-info"
+                     style={{top: this.props.top, left: this.props.left}}>
+                    <div className="card-header d-flex justify-content-between">
+                            Hello
+                            <button type="button" className={"btn btn-close"} onClick={() => this.props.close(this.props.modalId)}/>
+                    </div>
+                    <div className="card-body">
+                        Hello
+                    </div>
+                </div>
+            </Draggable>
         )
     }
 
